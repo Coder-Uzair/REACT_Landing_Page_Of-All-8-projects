a70980e2ca96412e22a33fff195234a6cd90e3ba
Coder-Uzair <githubuzair34@gmail.com>
2026-03-30 18:48:41 -0700

    Add files via upload

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,30 @@
+export default function LandingPage() {
+    return(
+        <>
+        <body>
+            <div className="background">
+             <div className="name"><h1 align="center"><em><strong>...All VIdeo Projects...</strong></em></h1></div>
+                <br/><br/><br/>
+            <div className="projects">
+                <h4><em><strong><a href= "https://react-video-project-1-wakf.vercel.app/">Video Project 1.. </a></strong></em></h4>
+                <br/><br/><br/><br/>
+                <h4><em><strong><a href="https://react-video-project-2.vercel.app/">Video Project 2..</a></strong></em></h4>
+                <br/><br/><br/><br/>
+                <h4><em><strong><a href="https://react-video-project-3.vercel.app/">Video Project 3..</a></strong></em></h4>
+                <br/><br/><br/><br/>
+                <h4><em><strong><a href="https://react-video-project-4.vercel.app/">Video Project 4..</a></strong></em></h4>
+                <br/><br/><br/><br/>
+                <h4><em><strong><a href="https://react-video-project-5.vercel.app/">Video Project 5..</a></strong></em></h4>
+                <br/><br/><br/><br/>
+                <h4><em><strong><a href="https://react-video-project-6.vercel.app/">Video Project 6..</a></strong></em></h4>
+                <br/><br/><br/><br/>
+                <h4><em><strong><a href="https://react-video-project-7.vercel.app/">Video Project 7..</a></strong></em></h4>
+                <br/><br/><br/><br/>
+                <h4><em><strong><a href="https://react-video-project-8-ea2m.vercel.app/">Video Project 8..</a></strong></em></h4>
+                <br/><br/><br/><br/>
+            </div>    
+            </div>
+        </body>
+        </>
+    );
+}
